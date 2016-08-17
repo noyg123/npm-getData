ad06dbe136d506696eb53531dc1d7bc82abd9b46
noy <noyg123@gmail.com>
2016-08-17 11:54:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+
+exports.getSources = function(callback) {
+    readJson("Sources.json", function(data) {
+        return callback(data);
+    });
+}
+exports.getTriangles = function(callback) {
+    readJson("Triangles.json", function(data){
+     return callback(data);
+    });
+}
+exports.getUsers = function(callback) {
+    readJson("Users.json", function(data){
+        return callback(data);
+    });
+}
+exports.getUserPermission = function(callback) {
+    readJson("UserPermission.json", function(data){
+        return callback(data);
+    });
+}
+exports.getC19nGroups = function(callback) {
+    readJson("C19nGroups.json", function(data){
+        return callback(data);
+    });
+}
+exports.getC19nGroups1 = function(callback) {
+    readJson("C19nGroups1.json", function(data){
+        return callback(data);
+    });
+}
+exports.getC19nGroups2 = function(callback) {
+    readJson("C19nGroups2.json", function(data){
+        return callback(data);
+    });
+}
+//getSources();
+//getPublishProceduresNohal();
+
+function readJson(pathFile, callback) {
+    var fs = require('fs');
+    var obj;
+    fs.readFile(__dirname + '/' + pathFile, 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    //console.log(obj);
+    return callback(obj);
+    });
+}
